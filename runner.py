6dab095ef76c6dcf22a5e444ee802b7aa451aee0
caitaozhan <caitaozhan@gmail.com>
2022-10-12 21:35:56 -0400

    minor

--- runner.py
+++ runner.py
@@ -3,34 +3,34 @@
 
 from subprocess import Popen, PIPE
 
-if  __name__ == '__main__':
-    
-    # plot 1: Y: accuracy, X: varying grid size
+
+def plot1():
+    # plot 1: POVMLoc (one level). Y: accuracy, X: varying grid size
     
                                   # length, size, noise, continuous
     template = 'python main.py -m {} -l {} -s {} -n {} -of {}'
     # 4 sensors
     config = [
-    #     ['POVM-Loc-One', 2, 4, 1,  'onelevel.4sen.varygrid'],
-    #     ['POVM-Loc-One', 4, 4, 1,  'onelevel.4sen.varygrid'],
-    #     ['POVM-Loc-One', 6, 4, 1,  'onelevel.4sen.varygrid'],
-    #     ['POVM-Loc-One', 8, 4, 1,  'onelevel.4sen.varygrid'],
-    #     ['POVM-Loc-One', 10, 4, 1, 'onelevel.4sen.varygrid'],
-    #     ['POVM-Loc-One', 12, 4, 1, 'onelevel.4sen.varygrid'],
-        # ['POVM-Loc-One', 14, 4, 1, 'onelevel.4sen.varygrid'],
-    #     ['POVM-Loc-One', 16, 4, 1, 'onelevel.4sen.varygrid']
+        ['POVM-Loc-One', 2, 4, 1,  'onelevel.4sen.varygrid'],
+        ['POVM-Loc-One', 4, 4, 1,  'onelevel.4sen.varygrid'],
+        ['POVM-Loc-One', 6, 4, 1,  'onelevel.4sen.varygrid'],
+        ['POVM-Loc-One', 8, 4, 1,  'onelevel.4sen.varygrid'],
+        ['POVM-Loc-One', 10, 4, 1, 'onelevel.4sen.varygrid'],
+        ['POVM-Loc-One', 12, 4, 1, 'onelevel.4sen.varygrid'],
+        ['POVM-Loc-One', 14, 4, 1, 'onelevel.4sen.varygrid'],
+        ['POVM-Loc-One', 16, 4, 1, 'onelevel.4sen.varygrid']
     ]
     # 8 sensors
-    config = [
+    # config = [
         # ['POVM-Loc-One', 2, 8, 1,  'onelevel.8sen.varygrid'],
         # ['POVM-Loc-One', 4, 8, 1,  'onelevel.8sen.varygrid'],
         # ['POVM-Loc-One', 6, 8, 1,  'onelevel.8sen.varygrid'],
         # ['POVM-Loc-One', 8, 8, 1,  'onelevel.8sen.varygrid'],
         # ['POVM-Loc-One', 10, 8, 1, 'onelevel.8sen.varygrid']
         # ['POVM-Loc-One', 12, 8, 1, 'onelevel.8sen.varygrid'],
-        ['POVM-Loc-One', 14, 8, 1, 'onelevel.8sen.varygrid'],
+        # ['POVM-Loc-One', 14, 8, 1, 'onelevel.8sen.varygrid'],
         # ['POVM-Loc-One', 16, 8, 1, 'onelevel.8sen.varygrid']
-    ]
+    # ]
 
     for i, c in enumerate(config):
         # if c[1] != 16:
@@ -39,3 +39,30 @@ if  __name__ == '__main__':
         print(command)
         p = Popen(command, shell=True)
         p.wait()
+
+
+
+def plot2():
+    # plot 2: POVMLoc and POVMLoc-Pro. Y: accuracy, X: varying grid size
+    
+                                  # length, size, noise, continuous
+    template = 'python main.py -m {} -l {} -n {} -of {}'
+    config = [
+        ['povmloc-pro', 16, 1, 'twolevel.varygrid']
+    ]
+
+    for i, c in enumerate(config):
+        # if c[1] != 16:
+        #     continue
+        command = template.format(c[0], c[1], c[2], c[3])
+        print(command)
+        p = Popen(command, shell=True)
+        p.wait()
+
+
+
+if  __name__ == '__main__':
+    
+    # plot1()
+    plot2()
+    

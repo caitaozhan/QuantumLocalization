417c7cbea08cb5b7fd3263d06fcbb752eb7e439d
caitaozhan <caitaozhan@gmail.com>
2022-10-14 18:07:41 -0400

    fix minor bug

--- localization.py
+++ localization.py
@@ -412,7 +412,7 @@ class QuantumLocalization:
         else:
             level_1_locerror = Utility.distance(tx_truth, tx_level1, Default.cell_length)
             print('level-1 tx', tx_level1, round(level_1_locerror, 3))
-            return level_1_locerror, tx_level1
+            return level_1_correct, level_1_locerror, tx_level1
 
 
     def training_twolevel_16x16grid(self):

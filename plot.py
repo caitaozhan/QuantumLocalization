6a039814b258faaa73e692515cc2ae1ab236c81b
caitaozhan <caitaozhan@gmail.com>
2023-04-29 21:50:44 -0400

    update plots

--- plot.py
+++ plot.py
@@ -9,20 +9,20 @@ from utility import Utility
 
 class Plot:
 
-    plt.rcParams['font.size'] = 45
+    plt.rcParams['font.size'] = 50
     plt.rcParams['lines.linewidth'] = 10
     plt.rcParams['lines.markersize'] = 20
     plt.rcParams['font.weight'] = 'bold'
     plt.rcParams['axes.labelweight'] = 'bold'
 
-    METHOD  = ['povmloc-one', 'povmloc',    'qml-r',   'qml-r-two',      'qml-c',   'qml-c-two',      'qml-ibmq_manila',        'qml-two-ibmq_manila']
-    _LEGEND = ['QSD-One',     'QSD-Two',    'PQC-One', 'PQC-Two',        'PQC-One', 'PQC-Two',        'PQC-One (IBM Quantum)', 'PQC-two (IBM Quantum)']
+    METHOD  = ['povmloc-one', 'povmloc',    'qml-r',    'qml-r-two',      'qml-c',    'qml-c-two',     'qml-ibmq_manila',       'qml-two-ibmq_manila']
+    _LEGEND = ['QSD-One',     'QSD-Two',    'PQC-One',  'PQC-Two',        'PQC-One',  'PQC-Two',       'PQC-One (IBM Quantum)', 'PQC-two (IBM Quantum)']
     LEGEND  = dict(zip(METHOD, _LEGEND))
 
-    _COLOR  = ['r',           'lightcoral', 'b',       'cornflowerblue', 'b',       'cornflowerblue', 'green',                 'springgreen']
+    _COLOR  = ['r',           'darksalmon', 'darkblue', 'cornflowerblue', 'darkblue', 'cornflowerblue', 'green',                'springgreen']
     COLOR   = dict(zip(METHOD, _COLOR))
 
-    _LINE   = ['-',           '--',         '-',        '--',            '-',        '--',            '-',                     '--']
+    _LINE   = ['-',           '--',         '-',        '--',             '-',        '--',             '-',                    '--']
     LINE    = dict(zip(METHOD, _LINE))
 
 
@@ -103,15 +103,15 @@ class Plot:
 
         # step 2: plotting
         l_err = "$L_{err}$"
-        fig, ax1 = plt.subplots(1, 1, figsize=(18, 16))
+        fig, ax1 = plt.subplots(1, 1, figsize=(22, 16))
         fig.subplots_adjust(left=0.13, right=0.98, top=0.91, bottom=0.12, wspace=0.13)
         ax1.plot(X_one, povmloc_one, linestyle=Plot.LINE['povmloc-one'], marker='o', label=Plot.LEGEND['povmloc-one'], mec='black', color=Plot.COLOR['povmloc-one'])
         ax1.plot(X_two, povmloc,     linestyle=Plot.LINE['povmloc'], marker='o', label=Plot.LEGEND['povmloc'],     mec='black', color=Plot.COLOR['povmloc'])
         ax1.plot(X_one, qml_r_one,   linestyle=Plot.LINE['qml-r'], marker='o', label=Plot.LEGEND['qml-r'],       mec='black', color=Plot.COLOR['qml-r'])
         ax1.plot(X_two, qml_r_two,   linestyle=Plot.LINE['qml-r-two'], marker='o', label=Plot.LEGEND['qml-r-two'],   mec='black', color=Plot.COLOR['qml-r-two'])
         # ax1
-        ax1.legend(ncol=1, handlelength=4, loc='upper left', fontsize=40)
-        ax1.set_xlabel('Grid Size', labelpad=10, fontsize=40)
+        ax1.legend(ncol=2, handlelength=3, loc='upper left')
+        ax1.set_xlabel('Grid Size', labelpad=25)
         ax1.grid(True)
         X = list(X_one)
         X.remove(9)
@@ -119,11 +119,10 @@ class Plot:
         ax1.set_xticklabels([f'{int(x)}x{int(x)}' for x in X])
         Y = list(range(0, 19, 3))
         ax1.set_yticks(Y)
-        ax1.tick_params(axis='x', pad=15, direction='in', length=10, width=5, labelsize=38, rotation=10)
+        ax1.tick_params(axis='x', pad=15, direction='in', length=10, width=5, labelsize=43)
         ax1.tick_params(axis='y', pad=15, direction='in', length=10, width=5)
-        ax1.set_ylabel(f'{l_err} (m)', labelpad=12)
-        # ax1.set_ylim([0, 40])
-        ax1.set_title(f'Performance of Localization Algorithms', pad=30, fontsize=45, fontweight='bold')
+        ax1.set_ylabel(f'{l_err} (m)', labelpad=15, fontsize=55)
+        ax1.set_title(f'Performance of Localization Algorithms', pad=30, fontweight='bold')
         fig.savefig(figname)
 
 
@@ -202,12 +201,10 @@ class Plot:
         qml_r_two   = arr[:, 4]
         X_one       = arr[:, 0]
 
-        # return
-
         # step 2: plotting
         l_err = "$L_{err}$"
-        fig, ax1 = plt.subplots(1, 1, figsize=(18, 16))
-        fig.subplots_adjust(left=0.14, right=0.98, top=0.91, bottom=0.11, wspace=0.13)
+        fig, ax1 = plt.subplots(1, 1, figsize=(22, 16))
+        fig.subplots_adjust(left=0.13, right=0.98, top=0.91, bottom=0.12, wspace=0.13)
         ind = np.arange(len(X_one))
         width = 0.15
         pos1 = ind - 1.5*width
@@ -219,15 +216,15 @@ class Plot:
         ax1.bar(pos3, qml_r_one,   width=width, edgecolor='black', label=Plot.LEGEND['qml-r'],       color=Plot.COLOR['qml-r'])
         ax1.bar(pos4, qml_r_two,   width=width, edgecolor='black', label=Plot.LEGEND['qml-r-two'],   color=Plot.COLOR['qml-r-two'])
         ax1.grid(True)
-        ax1.legend(ncol=1, handlelength=4, loc='upper right', fontsize=40)
-        ax1.set_xlabel('Sensor Number', labelpad=10, fontsize=40)
+        ax1.legend(ncol=2, handlelength=3, loc='upper right')
+        ax1.set_xlabel('Sensor Number', labelpad=10)
         X = list(X_one)
         ax1.set_xticks(ind)
         ax1.set_xticklabels([f'{int(x)}' for x in X])
         ax1.tick_params(axis='x', pad=15, length=10, width=5)
         ax1.tick_params(axis='y', pad=15, direction='in', length=10, width=5)
-        ax1.set_ylabel(f'{l_err} (m)', labelpad=20)
-        ax1.set_title(f'Localization Performance in a 16x16 Grid', pad=30, fontsize=45, fontweight='bold')
+        ax1.set_ylabel(f'{l_err} (m)', labelpad=20, fontsize=55)
+        ax1.set_title(f'Localization Performance in 16x16 Grid', pad=30, fontweight='bold')
         fig.savefig(figname)
 
 
@@ -253,8 +250,8 @@ class Plot:
             Y, bins, _ = plt.hist(error_list, n_bins, density=True, histtype='step', cumulative=True, label=method)
             method_n_bins.append((method, Y, bins))
         plt.close()
-        fig, ax = plt.subplots(figsize=(18, 16))
-        fig.subplots_adjust(left=0.15, right=0.96, top=0.9, bottom=0.12)
+        fig, ax = plt.subplots(figsize=(22, 16))
+        fig.subplots_adjust(left=0.13, right=0.96, top=0.91, bottom=0.12)
         for method, Y, bins in method_n_bins:
             ax.plot(bins[1:], Y, label=Plot.LEGEND[method], color=Plot.COLOR[method], linestyle=Plot.LINE[method])
         
@@ -262,9 +259,9 @@ class Plot:
         X = list(range(0, 41, 5))
         ax.set_xticks(X)
         ax.set_xticklabels([f'{int(x)}' for x in X])
-        ax.legend(loc='lower right', fontsize=40)
-        ax.set_xlabel('$L_{err}$ (m)', labelpad=20)
-        ax.set_ylabel('Percentage (%)', labelpad=20)
+        ax.legend(ncol=2, handlelength=3, loc='lower right')
+        ax.set_xlabel('$L_{err}$ (m)', labelpad=20, fontsize=55)
+        ax.set_ylabel('Percentage (%)', labelpad=20, fontsize=55)
         Y = np.linspace(0, 1, 6)
         ax.set_yticks(Y)
         ax.set_yticklabels([int(y*100) for y in Y])
@@ -272,7 +269,7 @@ class Plot:
         ax.set_xlim([0, 40])
         ax.tick_params(axis='x', pad=15, direction='in', length=10, width=5)
         ax.tick_params(axis='y', pad=15, direction='in', length=10, width=5)
-        ax.set_title('Cumulative Distribution Function of $L_{err}$', pad=30, fontsize=45, fontweight='bold')
+        ax.set_title('Cumulative Distribution Function of $L_{err}$', pad=30, fontweight='bold')
         fig.savefig(figname)
 
 
@@ -362,26 +359,26 @@ class Plot:
         X_two     = arr[:, 0]
 
         # step 2: plotting
-        fig, ax1 = plt.subplots(1, 1, figsize=(18, 16))
-        fig.subplots_adjust(left=0.15, right=0.98, top=0.91, bottom=0.12, wspace=0.13)
+        fig, ax1 = plt.subplots(1, 1, figsize=(22, 16))
+        fig.subplots_adjust(left=0.13, right=0.98, top=0.91, bottom=0.12, wspace=0.13)
         ax1.plot(X_one, povmloc_one, linestyle=Plot.LINE['povmloc-one'], marker='o', label=Plot.LEGEND['povmloc-one'], mec='black', color=Plot.COLOR['povmloc-one'])
         ax1.plot(X_two, povmloc,     linestyle=Plot.LINE['povmloc'], marker='o', label=Plot.LEGEND['povmloc'],     mec='black', color=Plot.COLOR['povmloc'])
         ax1.plot(X_one, qml_c_one,   linestyle=Plot.LINE['qml-c'], marker='o', label=Plot.LEGEND['qml-c'],       mec='black', color=Plot.COLOR['qml-c'])
         ax1.plot(X_two, qml_c_two,   linestyle=Plot.LINE['qml-c-two'], marker='o', label=Plot.LEGEND['qml-c-two'],   mec='black', color=Plot.COLOR['qml-c-two'])
         # ax1
-        ax1.legend(ncol=1, handlelength=4, loc='lower left', fontsize=40)
-        ax1.set_xlabel('Grid Size', labelpad=10, fontsize=40)
+        ax1.legend(ncol=2, handlelength=3, loc='lower left')
+        ax1.set_xlabel('Grid Size', labelpad=25)
         ax1.grid(True)
         X = list(X_one)
         X.remove(9)
         ax1.set_xticks(X)
         ax1.set_xticklabels([f'{int(x)}x{int(x)}' for x in X])
-        ax1.tick_params(axis='x', pad=15, direction='in', length=10, width=5, labelsize=38, rotation=10)
+        ax1.tick_params(axis='x', pad=15, direction='in', length=10, width=5, labelsize=43)
         ax1.tick_params(axis='y', pad=15, direction='in', length=10, width=5)
         CC_acc = "$CC_{acc}$"
-        ax1.set_ylabel(f'{CC_acc} (%)', labelpad=10)
+        ax1.set_ylabel(f'{CC_acc} (%)', labelpad=10, fontsize=55)
         ax1.set_ylim([0, 101.5])
-        ax1.set_title(f'Performance of Localization Algorithms', pad=30, fontsize=45, fontweight='bold')
+        ax1.set_title(f'Performance of Localization Algorithms', pad=30, fontweight='bold')
         fig.savefig(figname)
 
 
@@ -459,8 +456,8 @@ class Plot:
         X_one       = arr[:, 0]
 
         # step 2: plotting
-        fig, ax1 = plt.subplots(1, 1, figsize=(18, 18))
-        fig.subplots_adjust(left=0.14, right=0.98, top=0.8, bottom=0.11, wspace=0.13)
+        fig, ax1 = plt.subplots(1, 1, figsize=(22, 16))
+        fig.subplots_adjust(left=0.13, right=0.98, top=0.75, bottom=0.11, wspace=0.13)
         ind = np.arange(len(X_one))
         width = 0.15
         pos1 = ind - 1.5*width
@@ -472,8 +469,8 @@ class Plot:
         ax1.bar(pos3, qml_r_one,   width=width, edgecolor='black', label=Plot.LEGEND['qml-c'],       color=Plot.COLOR['qml-c'])
         ax1.bar(pos4, qml_r_two,   width=width, edgecolor='black', label=Plot.LEGEND['qml-c-two'],   color=Plot.COLOR['qml-c-two'])
         ax1.grid(True)
-        ax1.legend(ncol=2, handlelength=4, loc='upper center', fontsize=40, bbox_to_anchor=(0.5, 1.28))
-        ax1.set_xlabel('Sensor Number', labelpad=10, fontsize=40)
+        ax1.legend(ncol=2, handlelength=4, loc='upper center', bbox_to_anchor=(0.5, 1.4))
+        ax1.set_xlabel('Sensor Number', labelpad=10)
         X = list(X_one)
         ax1.set_xticks(ind)
         ax1.set_xticklabels([f'{int(x)}' for x in X])
@@ -481,8 +478,8 @@ class Plot:
         ax1.tick_params(axis='y', pad=15, direction='in', length=10, width=5)
         CC_acc = "$CC_{acc}$"
         ax1.set_ylim([0, 100])
-        ax1.set_ylabel(f'{CC_acc} (%)', labelpad=10)
-        ax1.set_title(f'Localization Performance in a 16x16 Grid', pad=30, fontsize=45, fontweight='bold')
+        ax1.set_ylabel(f'{CC_acc} (%)', labelpad=10, fontsize=55)
+        ax1.set_title(f'Localization Performance in 16x16 Grid', pad=30,fontweight='bold')
         fig.savefig(figname)
 
 
@@ -579,19 +576,20 @@ def runtime():
 
 if __name__ == '__main__':
 
-    # continuous_varygrid()
-    # continuous_varysensornum()
-    # localization_error_cdf()
+    continuous_varygrid()
+    continuous_varysensornum()
+    localization_error_cdf()
+    
+    discrete_varygrid()
+    discrete_varysensornum()
+    
+
+    # IBM    
     # continuous_varygrid_ibm()
     # localization_error_cdf_ibm()
-
-    discrete_varygrid()
-    # discrete_varysensornum()
     # discrete_varygrid_ibm()
 
 
-
-    # runtime()
 
 
 '''
@@ -633,6 +631,10 @@ Plot 2 -- continuous, fix grid size (16x16), all four methods, vary sensor
 
 Plot 3 -- continuous, error cdf
 
+method=povmloc-one, avg. error = 17.072553359683795, error std. = 12.614378742585068
+method=qml-r, avg. error = 8.527061522237194, error std. = 6.412694578444155
+method=povmloc, avg. error = 8.295051587301588, error std. = 12.750257170815887
+method=qml-r-two, avg. error = 4.8663696730640265, error std. = 7.507464855699436
 
 
 Plot 4 -- discrete, sensor=8, vary grid length, all four methods
@@ -668,13 +670,35 @@ Plot 5 -- discrete, grid length = 16, vary sensor, all four methods
               8       0.12549     0.765625  0.800781     0.972656
              16     nan         nan         0.949219     0.984375
 
+'''
 
+'''
              
-IBM -- CDF
+IBM Quantum Computer (not included in the paper)
 
-method=qml-noise=False, avg. error = 16.236119791666667, error std. = 5.907658642532727
-method=qml-noise=True, avg. error = 14.028208333333334, error std. = 6.785758629316787
+Plot 6 -- continous, onelevel
+  Grid Length    qml-r    qml-ibmq_manila
+-------------  -------  -----------------
+            2  1.67699            7.04915
+            3  2.04788            9.60601
+            4  3.30292           14.6689
+
+            
+Plot 7 -- continuous, CDF
+
 method=qml-r, avg. error = 3.3029218749999996, error std. = 3.069722834451804
+method=qml-ibmq_manila, avg. error = 14.66892670157068, error std. = 6.142534062312493
+method=qml-r-two, avg. error = 1.4385000000000001, error std. = 1.0174240041234857
+method=qml-two-ibmq_manila, avg. error = 16.723170454545457, error std. = 7.916616873446968
+
+
+Plot 8 -- discrete, onelevel
+
+  Grid Length    qml-c    qml-ibmq_manila
+-------------  -------  -----------------
+            2        1           1
+            3        1           0.585859
+            4        1           0.395833
 
 
 '''
